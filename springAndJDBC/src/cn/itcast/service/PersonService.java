f7d256d9c4d06d804d426182ae3c8dcb6250dc80
Administrator <Administrator@SHENG>
2021-04-21 23:44:37 +0800

    冲突处理

--- springAndJDBC/src/cn/itcast/service/PersonService.java
+++ springAndJDBC/src/cn/itcast/service/PersonService.java
@@ -5,10 +5,12 @@ import java.util.List;
 import cn.itcast.bean .Person;
 
 public interface PersonService {
-
+        
+	//处理冲突
 	public void delete(Integer personId);
 	public Person getPerson(Integer personId);
 	public List<Person> getPersons();
 	public void save(Person person);
 	public void update(Person person);
+	//处理冲突
 }
