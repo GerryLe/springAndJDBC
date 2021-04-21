b29970f4dc0e3fb7f8fc39efedbf2dbaccf68bd0
GerryLe <1980921973@qq.com>
2021-04-22 00:02:18 +0800

    Update PersonService.java

--- springAndJDBC/src/cn/itcast/service/PersonService.java
+++ springAndJDBC/src/cn/itcast/service/PersonService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import cn.itcast.bean .Person;
 
 public interface PersonService {
-        
+        //自己的代码
 	public void delete(Integer personId);
 	public Person getPerson(Integer personId);
 	public List<Person> getPersons();
